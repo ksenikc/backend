10904a1f8b810dcc70dc820c4884d2a938fec645
ksenikc_sa <61391548+ksenikc@users.noreply.github.com>
2024-03-06 14:05:00 +0300

    Add files via upload

---
+++ backend/modules/blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Article, Category
+from mptt.admin import DraggableMPTTAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'id', 'title', 'slug')
+    list_display_links = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+    fieldsets = (
+        ('Основная информация', {'fields': ('title', 'slug', 'parent')}),
+        ('Описание', {'fields': ('description',)})
+    )
+
+
+admin.site.register(Article)
